c93c822c00f40c36b686ba15d94656eb3e5844e6
erika-medeiros-accenture <erika.medeiros.silva@accenture.com>
2022-02-27 14:29:28 -0300

    criando securitu group e associando às intâncias EC2 já criadas

--- main.tf
+++ main.tf
@@ -15,7 +15,22 @@ resource "aws_instance" "dev" {
     tags = {
       "Name" = "dev${count.index}"
     }
+    vpc_security_group_ids = ["vpc-00fbb870d0a3f5da2"] #ID do security group (ID da VPC)
 }
 
 ## Criar security groups
 
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks = ["179.251.246.120/32"] # IP permitido - IP dinâmico (ver em: https://whatismyipaddress.com/)
+  }
+  tags = {
+    Name = "ssh"
+  }
+}
